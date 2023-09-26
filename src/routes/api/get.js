99aaeaf8766f8101d1ea1ca2f63ffa8ca18515ec
bhavik001 <bhmistry@myseneca.ca>
2023-09-26 14:45:04 -0400

    Implement step 42 for adding createSuccessResponse() and createErrorResponse() to some files

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,9 +1,6 @@
 // src/routes/api/get.js
 const { createSuccessResponse } = require('../../response');
 
-/**
- * Get a list of fragments for the current user
- */
 module.exports = (req, res) => {
   const fragments = [];
   const response = createSuccessResponse({ fragments });
