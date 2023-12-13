3432694da863cab73a91f270a6042741aae45bb1
bhavik001 <bhmistry@myseneca.ca>
2023-12-12 19:32:24 -0500

    Updating Post.js file

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -9,13 +9,6 @@ const url = process.env.API_URL;
 
 module.exports = async (req, res) => {
   try {
-    console.log(process.env.AWS_COGNITO_CLIENT_ID);
-    console.log(process.env.AWS_COGNITO_POOL_ID);
-    console.log(process.env.AWS_S3_BUCKET_NAME);
-    console.log(process.env.AWS_DYNAMODB_TABLE_NAME);
-    console.log(process.env.AWS_ACCESS_KEY_ID);
-    console.log(process.env.AWS_SECRET_ACCESS_KEY);
-    console.log(process.env.AWS_SESSION_TOKEN);
     logger.debug(`POST /v1/fragments called`);
     if (!Buffer.isBuffer(req.body)) {
       logger.warn(`POST /v1/fragments - Body requires correct data that is supported`);
@@ -47,13 +40,6 @@ module.exports = async (req, res) => {
     );
   } catch (err) {
     logger.error(`POST /v1/fragments - Internal Server Error: ${err}`);
-    console.log(process.env.AWS_COGNITO_CLIENT_ID);
-    console.log(process.env.AWS_COGNITO_POOL_ID);
-    console.log(process.env.AWS_S3_BUCKET_NAME);
-    console.log(process.env.AWS_DYNAMODB_TABLE_NAME);
-    console.log(process.env.AWS_ACCESS_KEY_ID);
-    console.log(process.env.AWS_SECRET_ACCESS_KEY);
-    console.log(process.env.AWS_SESSION_TOKEN);
     return res.status(500).json(response.createErrorResponse(500, err.message));
   }
 };
